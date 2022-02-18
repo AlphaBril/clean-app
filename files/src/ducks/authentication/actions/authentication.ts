6fdc01d3a7cef4194c01dba01b499100f38dea49
addy-dclxvi <addy4rock@gmail.com>
2022-02-19 00:07:00 +0100

    updated auth reducer

--- files/src/ducks/authentication/actions/authentication.ts
+++ files/src/ducks/authentication/actions/authentication.ts
@@ -1,5 +1,5 @@
 import { useMemo } from "react";
-import { useNavigate } from "react-router-dom";
+import { NavigateFunction, useNavigate } from "react-router-dom";
 import { useAppDispatch } from "src/hooks/hooks";
 import {
   loginSuccess,
@@ -8,10 +8,11 @@ import {
   AuthenticationState,
 } from "src/ducks/authentication/authenticationSlice";
 import { MessageState, setMessage } from "src/ducks/message/messageSlice";
-import axios from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
 
 import { SignupData } from "src/components/Auth/components/Signup/Signup.d";
 import { socket } from "src/hooks/useSocket";
+import { AppDispatch } from "src/store/configure";
 
 const PORT = 3001;
 const ADDRESS = "localhost";
@@ -27,14 +28,18 @@ const UPDATE_SURNAME_ENDPOINT = "/api/auth/surname";
 const UPDATE_NAME_ENDPOINT = "/api/auth/name";
 const RECOVER_PASSWORD_ENDPOINT = "/api/auth/recovery";
 
-const handleError = (dispatch: any, error: any) => {
-  const err = error.response.data.message || error.response.data.errno;
+const handleError = (dispatch: AppDispatch, error: AxiosError) => {
+  const err = error.response?.data.message || error.response?.data.errno;
   const message: MessageState = { value: err, status: "error" };
   dispatch(loginFailure());
   dispatch(setMessage(message));
 };
 
-const setUser = (dispatch: any, res: any, navigate: any) => {
+const setUser = (
+  dispatch: AppDispatch,
+  res: AxiosResponse,
+  navigate: NavigateFunction
+) => {
   const token = res.data.token;
   localStorage.setItem("user", token);
   const user: AuthenticationState = { isAuthenticated: true, user: token };
@@ -43,7 +48,7 @@ const setUser = (dispatch: any, res: any, navigate: any) => {
   navigate("/");
 };
 
-const userRegistrated = (dispatch: any, res: any, navigate: any) => {
+const userRegistrated = (dispatch: AppDispatch, navigate: NavigateFunction) => {
   const message: MessageState = {
     value: "Registration success, you need to validate your mail",
     status: "success",
@@ -52,7 +57,7 @@ const userRegistrated = (dispatch: any, res: any, navigate: any) => {
   navigate("/auth/login");
 };
 
-const userActivated = (dispatch: any, res: any) => {
+const userActivated = (dispatch: AppDispatch) => {
   const message: MessageState = {
     value: "Your account is now activated, please log in",
     status: "info",
@@ -60,7 +65,7 @@ const userActivated = (dispatch: any, res: any) => {
   dispatch(setMessage(message));
 };
 
-const passwordChanged = (dispatch: any, res: any) => {
+const passwordChanged = (dispatch: AppDispatch) => {
   const message: MessageState = {
     value: "Your password was updated, please log in",
     status: "info",
@@ -68,15 +73,7 @@ const passwordChanged = (dispatch: any, res: any) => {
   dispatch(setMessage(message));
 };
 
-const emailSent = (dispatch: any, res: any) => {
-  const message: MessageState = {
-    value: "An email has been sent",
-    status: "success",
-  };
-  dispatch(setMessage(message));
-};
-
-const emailChanged = (dispatch: any, res: any) => {
+const emailChanged = (dispatch: AppDispatch) => {
   const message: MessageState = {
     value: "Your email was updated",
     status: "info",
@@ -84,7 +81,7 @@ const emailChanged = (dispatch: any, res: any) => {
   dispatch(setMessage(message));
 };
 
-const usernameChanged = (dispatch: any, res: any) => {
+const usernameChanged = (dispatch: AppDispatch) => {
   const message: MessageState = {
     value: "Your username was updated",
     status: "info",
@@ -92,7 +89,7 @@ const usernameChanged = (dispatch: any, res: any) => {
   dispatch(setMessage(message));
 };
 
-const surnameChanged = (dispatch: any, res: any) => {
+const surnameChanged = (dispatch: AppDispatch) => {
   const message: MessageState = {
     value: "Your surname was updated",
     status: "info",
@@ -100,7 +97,7 @@ const surnameChanged = (dispatch: any, res: any) => {
   dispatch(setMessage(message));
 };
 
-const nameChanged = (dispatch: any, res: any) => {
+const nameChanged = (dispatch: AppDispatch) => {
   const message: MessageState = {
     value: "Your name was updated",
     status: "info",
@@ -108,7 +105,7 @@ const nameChanged = (dispatch: any, res: any) => {
   dispatch(setMessage(message));
 };
 
-const passswordChanged = (dispatch: any, res: any) => {
+const passswordChanged = (dispatch: AppDispatch) => {
   const message: MessageState = {
     value: "Your password was updated",
     status: "info",
@@ -116,124 +113,155 @@ const passswordChanged = (dispatch: any, res: any) => {
   dispatch(setMessage(message));
 };
 
-const login =
-  (username: string, password: string, navigate: any) => (dispatch: any) =>
-    axios.post(`${API_URL}${LOGIN_ENDPOINT}`, { username, password }).then(
-      (res) => {
-        setUser(dispatch, res, navigate);
-      },
-      (error) => {
-        handleError(dispatch, error);
-      }
-    );
+const login = (
+  dispatch: AppDispatch,
+  navigate: NavigateFunction,
+  username: string,
+  password: string
+) =>
+  axios.post(`${API_URL}${LOGIN_ENDPOINT}`, { username, password }).then(
+    (res) => {
+      setUser(dispatch, res, navigate);
+    },
+    (error) => {
+      handleError(dispatch, error);
+    }
+  );
 
-const logout = (navigate: any) => (dispatch: any) => {
+const logout = (dispatch: AppDispatch, navigate: NavigateFunction) => {
   dispatch(loginOut());
   localStorage.removeItem("user");
   navigate("/");
 };
 
-const signup =
-  ({ email, username, password, name, surname }: SignupData, navigate: any) =>
-  (dispatch: any) =>
-    axios
-      .post(`${API_URL}${SIGNUP_ENDPOINT}`, {
-        email,
-        username,
-        password,
-        name,
-        surname,
-      })
-      .then(
-        (res) => {
-          userRegistrated(dispatch, res, navigate);
-        },
-        (error) => {
-          handleError(dispatch, error);
-        }
-      );
-
-const activateUser = (token: string) => (dispatch: any) =>
-  axios.post(`${API_URL}${ACTIVATE_ENDPOINT}`, { token }).then(
-    (res) => {
-      userActivated(dispatch, res);
-    },
-    (error) => {
-      handleError(dispatch, error);
-    }
-  );
-
-const changePassword = (token: string, password: string) => (dispatch: any) =>
-  axios.post(`${API_URL}${CHANGE_PASSWORD_ENDPOINT}`, { token, password }).then(
-    (res) => {
-      passwordChanged(dispatch, res);
-    },
-    (error) => {
-      handleError(dispatch, error);
-    }
-  );
-
-const updatePassword =
-  (token: string | null, password: string) => (dispatch: any) =>
-    axios
-      .post(`${API_URL}${CHANGE_PASSWORD_ENDPOINT}`, { token, password })
-      .then(
-        (res) => {
-          passswordChanged(dispatch, res);
-        },
-        (error) => {
-          handleError(dispatch, error);
-        }
-      );
-
-const updateEmail = (token: string | null, email: string) => (dispatch: any) =>
-  axios.post(`${API_URL}${UPDATE_EMAIL_ENDPOINT}`, { token, email }).then(
-    (res) => {
-      emailChanged(dispatch, res);
-    },
-    (error) => {
-      handleError(dispatch, error);
-    }
-  );
-
-const updateUsername =
-  (token: string | null, username: string) => (dispatch: any) =>
-    axios
-      .post(`${API_URL}${UPDATE_USERNAME_ENDPOINT}`, { token, username })
-      .then(
-        (res) => {
-          usernameChanged(dispatch, res);
-        },
-        (error) => {
-          handleError(dispatch, error);
-        }
-      );
-
-const updateSurname =
-  (token: string | null, surname: string) => (dispatch: any) =>
-    axios.post(`${API_URL}${UPDATE_SURNAME_ENDPOINT}`, { token, surname }).then(
-      (res) => {
-        surnameChanged(dispatch, res);
+const signup = (
+  dispatch: AppDispatch,
+  navigate: NavigateFunction,
+  { email, username, password, name, surname }: SignupData
+) =>
+  axios
+    .post(`${API_URL}${SIGNUP_ENDPOINT}`, {
+      email,
+      username,
+      password,
+      name,
+      surname,
+    })
+    .then(
+      () => {
+        userRegistrated(dispatch, navigate);
       },
       (error) => {
         handleError(dispatch, error);
       }
     );
 
-const updateName = (token: string | null, name: string) => (dispatch: any) =>
-  axios.post(`${API_URL}${UPDATE_NAME_ENDPOINT}`, { token, name }).then(
-    (res) => {
-      nameChanged(dispatch, res);
+const activateUser = (dispatch: AppDispatch, token: string) =>
+  axios.post(`${API_URL}${ACTIVATE_ENDPOINT}`, { token }).then(
+    () => {
+      userActivated(dispatch);
     },
     (error) => {
       handleError(dispatch, error);
     }
   );
 
-const passwordRecovery = (email: string) => (dispatch: any) =>
+const changePassword = (
+  dispatch: AppDispatch,
+  token: string,
+  password: string
+) =>
+  axios.post(`${API_URL}${CHANGE_PASSWORD_ENDPOINT}`, { token, password }).then(
+    () => {
+      passwordChanged(dispatch);
+    },
+    (error) => {
+      handleError(dispatch, error);
+    }
+  );
+
+const updatePassword = (
+  dispatch: AppDispatch,
+  token: string | null,
+  password: string
+) =>
+  axios.post(`${API_URL}${CHANGE_PASSWORD_ENDPOINT}`, { token, password }).then(
+    () => {
+      passswordChanged(dispatch);
+    },
+    (error) => {
+      handleError(dispatch, error);
+    }
+  );
+
+const updateEmail = (
+  dispatch: AppDispatch,
+  token: string | null,
+  email: string
+) =>
+  axios.post(`${API_URL}${UPDATE_EMAIL_ENDPOINT}`, { token, email }).then(
+    () => {
+      emailChanged(dispatch);
+    },
+    (error) => {
+      handleError(dispatch, error);
+    }
+  );
+
+const updateUsername = (
+  dispatch: AppDispatch,
+  token: string | null,
+  username: string
+) =>
+  axios.post(`${API_URL}${UPDATE_USERNAME_ENDPOINT}`, { token, username }).then(
+    () => {
+      usernameChanged(dispatch);
+    },
+    (error) => {
+      handleError(dispatch, error);
+    }
+  );
+
+const updateSurname = (
+  dispatch: AppDispatch,
+  token: string | null,
+  surname: string
+) =>
+  axios.post(`${API_URL}${UPDATE_SURNAME_ENDPOINT}`, { token, surname }).then(
+    () => {
+      surnameChanged(dispatch);
+    },
+    (error) => {
+      handleError(dispatch, error);
+    }
+  );
+
+const updateName = (
+  dispatch: AppDispatch,
+  token: string | null,
+  name: string
+) =>
+  axios.post(`${API_URL}${UPDATE_NAME_ENDPOINT}`, { token, name }).then(
+    () => {
+      nameChanged(dispatch);
+    },
+    (error) => {
+      handleError(dispatch, error);
+    }
+  );
+
+const emailSent = (dispatch: AppDispatch) => {
+  const message: MessageState = {
+    value: "An email has been sent",
+    status: "success",
+  };
+  dispatch(setMessage(message));
+};
+
+const passwordRecovery = (dispatch: AppDispatch, email: string) =>
   axios.post(`${API_URL}${RECOVER_PASSWORD_ENDPOINT}`, { email }).then(
-    (res) => {
-      emailSent(dispatch, res);
+    () => {
+      emailSent(dispatch);
     },
     (error) => {
       handleError(dispatch, error);
@@ -241,29 +269,29 @@ const passwordRecovery = (email: string) => (dispatch: any) =>
   );
 
 export const useAuthentication = () => {
-  const dispatch = useAppDispatch();
+  const dispatch: AppDispatch = useAppDispatch();
   const navigate = useNavigate();
 
   return useMemo(
     () => ({
-      activateUser: (token: string) => dispatch(activateUser(token)),
-      passwordRecovery: (email: string) => dispatch(passwordRecovery(email)),
+      activateUser: (token: string) => activateUser(dispatch, token),
+      passwordRecovery: (email: string) => passwordRecovery(dispatch, email),
       changePassword: (token: string, password: string) =>
-        dispatch(changePassword(token, password)),
+        changePassword(dispatch, token, password),
       updatePassword: (token: string | null, password: string) =>
-        dispatch(updatePassword(token, password)),
+        updatePassword(dispatch, token, password),
       updateEmail: (token: string | null, email: string) =>
-        dispatch(updateEmail(token, email)),
+        updateEmail(dispatch, token, email),
       updateUsername: (token: string | null, username: string) =>
-        dispatch(updateUsername(token, username)),
+        updateUsername(dispatch, token, username),
       updateSurname: (token: string | null, surname: string) =>
-        dispatch(updateSurname(token, surname)),
+        updateSurname(dispatch, token, surname),
       updateName: (token: string | null, name: string) =>
-        dispatch(updateName(token, name)),
+        updateName(dispatch, token, name),
       login: (username: string, password: string) =>
-        dispatch(login(username, password, navigate)),
-      signup: (data: SignupData) => dispatch(signup(data, navigate)),
-      logout: () => dispatch(logout(navigate)),
+        login(dispatch, navigate, username, password),
+      signup: (data: SignupData) => signup(dispatch, navigate, data),
+      logout: () => logout(dispatch, navigate),
     }),
     [dispatch, navigate]
   );
